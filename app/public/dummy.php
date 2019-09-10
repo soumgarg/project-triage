6ee98f9f41ec27d4b64163adc54970a49299e8ab
Soumya Garg <soumya.vi@gmail.com>
2019-09-10 17:25:24 -0400

    Adding dummy.php

---
+++ app/public/dummy.php
@@ -0,0 +1,31 @@
+{
+	"patient": [{
+			"patientGuid": "PATIENT-ID-123",
+			"firstName": "Lenny",
+			"lastName": "Sage",
+			"dob": "02/15/1997",
+			"sexAtBirth": "F"
+		},
+		{
+			"patientGuid": "PATIENT-ID-456",
+			"firstName": "Paris",
+			"lastName": "Dennis",
+			"dob": "10/20/1993",
+			"sexAtBirth": "F"
+		},
+        {
+			"patientGuid": "PATIENT-ID-789",
+			"firstName": "Jason",
+			"lastName": "Choi",
+			"dob": "05/24/1994",
+			"sexAtBirth": "M"
+		},
+        {
+			"patientGuid": "PATIENT-ID-987",
+			"firstName": "Cade",
+			"lastName": "Stewart",
+			"dob": "11/11/2000",
+			"sexAtBirth": "M"
+		}
+	]
+}
